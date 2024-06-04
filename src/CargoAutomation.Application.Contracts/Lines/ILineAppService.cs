22ca1974cacd4c60ca5e68104ddde37d20084376
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-06-04 17:23:31 +0300

    ILineAppService Sınıfı oluşturuldu

---
+++ src/CargoAutomation.Application.Contracts/Lines/ILineAppService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Dtos.Lines;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace CargoAutomation.Lines
+{
+    public interface ILineAppService : IApplicationService
+    {
+        Task<ListResultDto<LineDto>> GetListAsync();
+        Task<LineDto> GetAsync(int id);
+        Task<LineDto> CreateAsync(CreateLineDto input);
+        Task UpdateAsync(int id, UpdateLineDto input);
+        Task DeleteAsync(int id);
+        Task SoftDeleteAsync(int id);
+    }
+}
