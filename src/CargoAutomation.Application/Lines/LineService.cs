f5b0c734b26a427f714740e6ae99f4df68c75a84
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2024-06-05 17:44:37 +0300

    LineService oluşturuldu

---
+++ src/CargoAutomation.Application/Lines/LineService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using CargoAutomation.Lines;
+using Entities.Dtos.Lines;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace CargoAutomation.Application
+{
+    public class LineService : ApplicationService, ILineAppService
+    {
+        private readonly LineManager _lineManager;
+
+        public LineService(LineManager lineManager)
+        {
+            _lineManager = lineManager;
+        }
+
+        public async Task<LineDto> CreateAsync(CreateLineDto input)
+        {
+            return await _lineManager.CreateAsync(input);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _lineManager.DeleteAsync(id);
+        }
+
+        public async Task<LineDto> GetAsync(Guid id)
+        {
+            return await _lineManager.GetAsync(id);
+        }
+
+        public async Task<PagedResultDto<LineDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+        {
+            return await _lineManager.GetListAsync(input);
+        }
+
+        public async Task<LineDto> UpdateAsync(Guid id, UpdateLineDto input)
+        {
+            return await _lineManager.UpdateAsync(id, input);
+        }
+
+        public async Task SoftDeleteAsync(Guid id)
+        {
+            await _lineManager.SoftDeleteAsync(id);
+        }
+    }
+}
